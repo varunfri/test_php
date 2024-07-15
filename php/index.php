3abddad8430df6626c0544c83d66695eb855d8aa
Varun K V <varunvishwanath1@gmail.com>
2024-07-15 21:42:25 +0530

    Create index.php

---
+++ php/index.php
@@ -0,0 +1,10 @@
+<?php 
+echo "<html>";
+echo "<head>";
+echo "<title> Test PHP Pages </title>";
+echo "</head>";
+echo "<body>";
+echo "<h1>Testing the PHP file with github pages</h1>";
+echo "</body>";
+echo"</html>";
+?>
